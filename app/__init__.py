525189af48de790392b691a33a1b6a9a6ca2e61c
zain azhar <142168421+ZAINAZHAR303@users.noreply.github.com>
2025-04-29 19:56:22 +0500

    remove streamlit ui & fast api routes

--- app/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-from .api import api_router
-
-app.include_router(api_router)
--- app/core/__init__.py
+++
@@ -1,2 +0,0 @@
-# app/core/__init__.py
-# This file is used to initialize the core module.
